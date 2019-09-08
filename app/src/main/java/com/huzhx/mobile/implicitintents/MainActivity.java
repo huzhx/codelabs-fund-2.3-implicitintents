bff5a047a3d98df783f94ced381a52bafd986b75
Zhaoxian Hu <huzhx@uci.edu>
2019-09-08 15:23:44 -0700

    implement the open location button

--- app/src/main/java/com/huzhx/mobile/implicitintents/MainActivity.java
+++ app/src/main/java/com/huzhx/mobile/implicitintents/MainActivity.java
@@ -12,12 +12,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
     private EditText mWebsiteEditText;
+    private EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEditText = findViewById(R.id.website_edittext);
+        mLocationEditText = findViewById(R.id.location_edittext);
     }
 
     public void openWebsite(View view) {
@@ -34,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openLocation(View view) {
+        String loc = mLocationEditText.getText().toString();
+
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "Can't handle this intent!");
+        }
     }
 
     public void shareText(View view) {
